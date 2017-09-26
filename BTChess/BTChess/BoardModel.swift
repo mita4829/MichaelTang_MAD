cb0a165bd07ab210d6bd665164246d79f494439d
Michael Tang <michael.tang-1@colorado.edu>
2017-09-26 11:02:37 -0600

    Add writeup and images

---
+++ BTChess/BTChess/BoardModel.swift
@@ -0,0 +1,9 @@
+//
+//  BoardModel.swift
+//  BTChess
+//
+//  Created by Michael Tang on 9/24/17.
+//  Copyright © 2017 Michael Tang. All rights reserved.
+//
+
+import Foundation
---
+++ BTChess/BTChess/BoardSquare.swift
@@ -0,0 +1,13 @@
+//
+//  BoardSquare.swift
+//  BTChess
+//
+//  Created by Michael Tang on 9/24/17.
+//  Copyright © 2017 Michael Tang. All rights reserved.
+//
+
+import UIKit
+
+class BoardSquare: UICollectionViewCell {
+    
+}
